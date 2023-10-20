a0d86f5a816737155e15a8d6bd35262c42f97796
GRISVARD Bastien <bgrisvar@etudiant.univ-lr.fr>
2023-10-20 20:53:22 +0200

    Remove code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import { ArticleList } from "@/components/server-components/ArticleList";
-import { Suspense } from "react";
-import Loading from "@/app/loading";
 
 export default async function Home() {
   return (
     <>
       <div className="flex justify-center">
-        <Suspense fallback={<Loading />}>
-          <ArticleList />
-        </Suspense>
+        <ArticleList />
       </div>
     </>
   );
