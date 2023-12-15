21313c2e46e0aeb9ea47a67213d8c90fcb510007
GRISVARD Bastien <bgrisvar@etudiant.univ-lr.fr>
2023-12-15 13:35:43 +0100

    remove zod verif

--- src/services/createNewArticle.ts
+++ src/services/createNewArticle.ts
@@ -1,10 +1,3 @@
 "use server";
-import { z } from "zod";
 
-const schema = z.object({
-  name: z.string().min(1),
-  description: z.string().min(1),
-  content: z.string().min(1),
-});
-
-export async function createNewArticle(prevState: any, formData: FormData) {}
+export async function createNewArticle() {}
